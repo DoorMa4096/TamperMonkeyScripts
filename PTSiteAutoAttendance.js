e433efd541262a0ad281d549a96cab34609ef654
4096 <77264951+DoorMa4096@users.noreply.github.com>
2025-07-11 13:13:52 +0800

    Update PTSiteAutoAttendance.js

--- PTSiteAutoAttendance.js
+++ PTSiteAutoAttendance.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PT site auto attendance (self use)
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.1.3
 // @description  每天凌晨0点在pt站点进行签到
 // @author       Door Ma
 // @match        https://pt.btschool.club/*
