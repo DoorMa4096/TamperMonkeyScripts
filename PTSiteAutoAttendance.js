9218e03ceeeaef95dd2edcbb770b6f0bc1617745
4096 <77264951+DoorMa4096@users.noreply.github.com>
2025-07-12 09:08:56 +0800

    Update PTSiteAutoAttendance.js

--- PTSiteAutoAttendance.js
+++ PTSiteAutoAttendance.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PT site auto attendance (self use)
 // @namespace    http://tampermonkey.net/
-// @version      0.1.7
+// @version      0.1.8
 // @description  每天凌晨0点在pt站点进行签到
 // @author       Door Ma
 // @match        https://pt.btschool.club/*
@@ -47,7 +47,7 @@
 
     // 首次调度，并在每次触发后重新调度
     function scheduleDaily() {
-        const localPtSiteURL = URL(window.location.href);
+        const localPtSiteURL = new URL(window.location.href);
         console.log('[签到脚本] 当前站点', localPtSiteURL.host);
         const localPtSite = ptSites.find(e => {e.domain === localPtSiteURL.host});
         console.log('[签到脚本] 当前站点信息', localPtSite);
