49228c4029fae42bdc4f823090b83626b5e10a17
4096 <77264951+DoorMa4096@users.noreply.github.com>
2025-07-12 09:00:39 +0800

    Update PTSiteAutoAttendance.js
    add debug flag

--- PTSiteAutoAttendance.js
+++ PTSiteAutoAttendance.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PT site auto attendance (self use)
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.5
 // @description  每天凌晨0点在pt站点进行签到
 // @author       Door Ma
 // @match        https://pt.btschool.club/*
@@ -49,6 +49,7 @@
     function scheduleDaily() {
         const localPtSiteDomain = window.location.href.host;
         const localPtSite = ptSites.find(e => {e.domain === localPtSiteDomain});
+        console.log('[签到脚本] 当前站点', localPtSite);
         const delay = getDelayToNextMidnight();
         clickSign(localPtSite);
         console.log('[签到脚本] 下次将在', delay / 1000 / 60, '分钟后执行签到。');
