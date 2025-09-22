ff584f3f08f8164ea9137812500f6c75c09e9c19
4096 <77264951+DoorMa4096@users.noreply.github.com>
2025-09-22 16:31:16 +0800

    Update PTSiteAutoAttendance.js

--- PTSiteAutoAttendance.js
+++ PTSiteAutoAttendance.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PT site auto attendance (self use)
 // @namespace    http://tampermonkey.net/
-// @version      0.1.14
+// @version      0.1.15
 // @description  每天凌晨0点在pt站点进行签到
 // @author       Door Ma
 // @match        https://pt.btschool.club/*
@@ -23,12 +23,13 @@
     }];
     
     // 点击签到链接
-    function clickSign(site) {
+    async function clickSign(site) {
         const link = document.querySelector('a[href="' + site.attendenceUrl + '"]');
         if (link) {
             link.click();
             console.log('[签到脚本] 已于', new Date().toLocaleString(), '执行点击。');
             localStorage.setItem('checkedDate', new Date().toDateString()); // 标志签到时间
+            await wait(1000);
         } else {
             console.warn('[签到脚本] 未找到签到按钮。');
         }
@@ -52,14 +53,13 @@
     }
     
     // 首次调度，并在每次触发后重新调度
-    async function scheduleDaily() {
+    function scheduleDaily() {
         const localPtSiteURL = new URL(window.location.href);
         console.log('[签到脚本] 当前站点', localPtSiteURL.host);
         const localPtSite = ptSites.find(e => e.domain === localPtSiteURL.host);
         console.log('[签到脚本] 当前站点信息', localPtSite);
         while (localStorage.getItem('checkedDate') !== new Date().toDateString()) {
-            clickSign(localPtSite);
-            await wait(1000);
+            await clickSign(localPtSite);
         }
         const delay = getDelayToNextMidnight();
         console.log('[签到脚本] 下次将在', delay / 1000 / 60, '分钟后执行签到。');
